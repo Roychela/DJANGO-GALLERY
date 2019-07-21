f19a1dcaf84d225e8a48697c808d40f786a01253
Roy Chela <roychela@gmail.com>
2019-07-21 18:17:38 +0300

    Add location class and its test

--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location_name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.location_name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(location_name=value)
--- personal/tests.py
+++ personal/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
-
+from .models import Location
 # Create your tests here.
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(location_name='Paris')
+        self.location.save()
+
+    def tearDown(self):
+        Location.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location,Location))
+
+    def test_save_location(self):
+        Location.objects.all().delete()
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
+    def test_update_location(self):
+        new_location_name = 'India'
+        self.location.update_location(self.location.id,new_location_name)
+        updated_location = Location.objects.filter(location_name='India')
+        self.assertTrue(len(updated_location)>0)
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location)==0)
