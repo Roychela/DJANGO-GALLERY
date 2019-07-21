b38bbea97ba3522f9e6c748ca3bade6702629dbd
Roy Chela <roychela@gmail.com>
2019-07-21 19:15:17 +0300

    Add search image view function

--- personal/views.py
+++ personal/views.py
@@ -4,3 +4,18 @@ from .models import Image, Category, Location
 def welcome(request):
     images = Image.get_all_images()
     return render(request, 'welcome.html', {"images": images})
+
+def search_image(request):
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    if 'image' in request.GET and request.GET['image']:
+        search_term = request.GET.get('image')
+        images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',
+                      {'images': images, 'message': message, 'categories': categories,
+                       "locations": locations})
+    else:
+        message = 'You havent searched yet'
+        return render(request, 'search.html', {"message": message})
