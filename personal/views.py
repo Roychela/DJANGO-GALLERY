67abf3a49ec5c0a18edf2bd1765a435e99f73d34
Roy Chela <roychela@gmail.com>
2019-07-21 18:51:21 +0300

    Add welcome view function

--- personal/views.py
+++ personal/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Image, Category, Location
 # Create your views here.
+def welcome(request):
+    images = Image.get_all_images()
+    return render(request, 'welcome.html', {"images": images})
